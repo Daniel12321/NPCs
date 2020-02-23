a9f639b30707ed7264870ef7ab5ae593d629ae0b
Daniel <danielpeters9797@gmail.com>
2020-02-23 22:31:49 +0100

    Remove Mixins that are no longer needed

--- src/main/java/me/mrdaniel/npcs/mixin/MixinEntity.java
+++ src/main/java/me/mrdaniel/npcs/mixin/MixinEntity.java
@@ -6,25 +6,10 @@ import net.minecraft.util.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Entity.class, priority = 10)
 public abstract class MixinEntity {
-
-	@Inject(method = "setFire", at = @At("HEAD"), cancellable = true)
-	public void onSetFire(final int seconds, CallbackInfo ci) {
-		if (this.isNPC()) {
-			ci.cancel();
-		}
-	}
-
-	@Inject(method = "isPushedByWater", at = @At("RETURN"), cancellable = true)
-	public void onIsPushedByWater(final CallbackInfoReturnable<Boolean> cir) {
-		if (this.isNPC() && cir.getReturnValue()) {
-			cir.setReturnValue(false);
-		}
-    }
 
 	@Inject(method = "isEntityInvulnerable", at = @At("RETURN"), cancellable = true)
 	public void onIsEntityInvulnerable(final DamageSource source, final CallbackInfoReturnable<Boolean> cir) {
